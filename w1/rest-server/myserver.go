65a8985ccb9f53a3e595af965b7a344262c17348
ByoungSeob Kim <46367962+powerkimhub@users.noreply.github.com>
2020-08-08 12:00:49 +0900

    Create myserver.go

---
+++ w1/rest-server/myserver.go
@@ -0,0 +1,102 @@
+// Rest Runtime Server for CB-Contributhon Edu.
+//
+//      * Cloud-Barista: https://github.com/cloud-barista
+//
+// by powerkim, 2020.08.
+
+package main
+
+import (
+        "fmt"
+
+        // for log print
+        "github.com/sirupsen/logrus"
+        "github.com/cloud-barista/cb-log"
+
+        // for REST Server (echo)
+        "github.com/labstack/echo"
+        "github.com/labstack/echo/middleware"
+        "net/http"
+)
+
+var logger *logrus.Logger
+func init() {
+        // cblog is a global variable.
+        logger = cblog.GetLogger("CB-Contributhon")
+}
+
+// REST API Return struct for boolena type
+//====================================================================
+type MyInfo struct {
+        Name string `json:"name"`
+        Github string `json:"github"`
+}
+
+//ex) {"POST", "/driver", registerCloudDriver}
+type route struct {
+        method, path string
+        function     echo.HandlerFunc
+}
+
+func main() {
+
+        //======================================= setup routes
+        routes := []route{
+
+                //----------MyInfo
+                //{"GET", "/driver/:DriverName", getCloudDriver},
+                {"GET", "/2020/myinfo", getMyInfo},
+                /*
+                {"POST", "/driver", registerCloudDriver},
+                {"GET", "/driver", listCloudDriver},
+                {"DELETE", "/driver/:DriverName", unRegisterCloudDriver},
+
+                */
+                //-------------------------------------------------------------------//
+        }
+        //======================================= setup routes
+
+        fmt.Println("\n[CB-Contributhon:Test REST Framework]")
+        fmt.Println("\n   Initiating REST Server....__^..^__....\n\n")
+
+        // Run REST Server
+        ApiServer(routes, ":8080")
+}
+
+//================ REST Server: setup & start
+func ApiServer(routes []route, strPort string) {
+        e := echo.New()
+
+        // Middleware
+        e.Use(middleware.CORS())
+        e.Use(middleware.Logger())
+        e.Use(middleware.Recover())
+
+        for _, route := range routes {
+                switch route.method {
+                case "POST":
+                        e.POST(route.path, route.function)
+                case "GET":
+                        e.GET(route.path, route.function)
+                case "PUT":
+                        e.PUT(route.path, route.function)
+                case "DELETE":
+                        e.DELETE(route.path, route.function)
+
+                }
+        }
+
+        e.HideBanner = true
+        if strPort == "" {
+                strPort = ":8080"
+        }
+        e.Logger.Fatal(e.Start(strPort))
+}
+
+//================ List of support CloudOS
+func getMyInfo(c echo.Context) error {
+        logger.Info("call getMhyInfo()")
+
+        myInfo := MyInfo {"powerkim", "powerkimhub"}
+        return c.JSON(http.StatusOK, &myInfo)
+}
